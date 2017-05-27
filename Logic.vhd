0855649bf0cf42cfc43f9664432bd20ffa467d74
Xavier Yao <xavieryao@live.cn>
2017-05-27 12:02:11 +0800

    game round & win condition

--- Logic.vhd
+++ Logic.vhd
@@ -18,9 +18,15 @@ end entity;
 
 architecture bhv of Logic is
 	signal count: std_logic_vector(24 downto 0);
+	signal pea_clk_count : std_logic_vector(5 downto 0);
 	signal pea_clk, zombie_clk: std_logic;
 	signal plants: plant_vector := (("00", "1010", M, '0', "0000"), ("00", "1010", M, '0', "0000"), others => ("00", "0000", M, '0', "0000"));
 	signal zombies: zombie_vector := (("1010", 15), ("1010", M-1), others => ("0000", 0));
+	signal passed_round : integer := 0; -- 过去了多少轮
+
+	signal zombies_to_update : std_logic_vector(0 to N-1); -- 需要更新x的僵尸
+	constant ROUND_CLK : integer := 20;
+	constant WIN_CONDITION : integer := 10; -- 需要过10轮才能赢
 begin
 	out_zombies <= zombies;
 	out_plants <= plants;
@@ -48,6 +54,7 @@ begin
 	-- 处理豌豆
 	process(pea_clk)
 		variable p: plant;
+		variable has_win : std_logic := '0';
 	begin
 		if (pea_clk'event and pea_clk = '1') then
 			for i in 0 to N-1 loop
@@ -86,6 +93,28 @@ begin
 					end if;
 				end loop;
 			end loop;
+
+			-- 更新僵尸
+			if pea_clk_count=ROUND_CLK then
+				pea_clk_count <= (others => '0');
+				if passed_round = WIN_CONDITION then
+					has_win := '1';
+				else
+					for i in 0 to N-1 loop
+						if zombies(i).hp = 0 then
+							zombies(i).hp <= "1010";
+							zombies_to_update(i) <= '1';
+							passed_round <= passed_round + 1;
+						end if;
+					end loop;
+					has_win := '0';
+				end if;
+			else
+				pea_clk_count <= pea_clk_count + 1;
+				zombies_to_update <= (others => '0');
+			end if;
+			out_win <= has_win;
+
 		end if;
 	end process;
 
@@ -97,7 +126,9 @@ begin
 	begin
 		if (zombie_clk'event and zombie_clk = '1') then
 			for i in 0 to N-1 loop
-				if (zombies(i).hp > 0) then
+				if (zombies_to_update(i) = '1') then
+					zombies(i).x <= M-1;
+				elsif (zombies(i).hp > 0) then
 					if (plants(i * M + zombies(i).x).hp > 0) then
 						if (plants(i * M + zombies(i).x).plant_type="10") then -- 坚果墙的防御力较高，特殊处理
 							plants(i * M + zombies(i).x).hp <= plants(i * M + zombies(i).x).hp - NUT_HARM;
@@ -109,14 +140,15 @@ begin
 					end if;
 				end if;
 			end loop;
+
+			for i in 0 to N-1 loop
+				if (zombies(i).hp > 0 and zombies(i).x = 0 and plants(i * M + zombies(i).x).hp = 0) then
+					has_lost := '1';
+				end if;
+			end loop;
+			out_lost <= has_lost;
 		end if;
 
-		for i in 0 to N-1 loop
-			if (zombies(i).hp > 0 and zombies(i).x = 0 and plants(i * M + zombies(i).x).hp = 0) then
-				has_lost := '1';
-			end if;
-		end loop;
-		out_lost <= has_lost;
 	end process;
 
 end architecture;
